87197bcd279bd61dc6873aae1e8efd08eb2bd574
Serhii Posternak <posternak.serhii@airslate.com>
2022-03-03 15:00:47 +0200

    add cron

--- automation/digitaocean/auto-creating-nine/auto9.sh
+++ automation/digitaocean/auto-creating-nine/auto9.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+mkdir /root/.config
 snap install doctl
 doctl auth init --access-token $API_KEY
-../clear.sh
-../init.sh 9
+./stop-russia/automation/digitaocean/clear.sh
+./stop-russia/automation/digitaocean/init.sh 9
+
+# cron
+apt install cron
+systemctl enable cron
+(crontab -l 2>/dev/null; echo "* 3 * * * ./stop-russia/automation/digitaocean/recreate_droplets.sh") | crontab -
---
+++ automation/digitaocean/auto-creating-nine/recreate_droplets.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./stop-russia/automation/digitaocean/clear.sh
+./stop-russia/automation/digitaocean/init.sh 9
